f7ff3b0ad4e9d266ec300c1f6fe8f0227af5d0c2
Sucram314 <99621225+Sucram314@users.noreply.github.com>
2023-08-14 15:33:34 -0400

    epic navigation bar

---
+++ scripts/header.js
@@ -0,0 +1,30 @@
+const header = document.getElementById("header");
+let lastScroll = 0;
+let height = header.offsetHeight;
+
+let didScroll = false;
+
+window.onscroll = function (e){ 
+    didScroll = true;
+};
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    let st = scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+    if(st*2 <= height+lastScroll){
+        header.classList.remove("down");
+        header.classList.add("up");
+    } else {
+        header.classList.remove("up");
+        header.classList.add("down");
+    }
+
+    lastScroll = st;
+}
